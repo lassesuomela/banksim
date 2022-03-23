c1d420ac60957a85097e228ff38862beef877f39
Tommukka <laine.tomi01@gmail.com>
2022-03-23 13:45:49 +0200

    delete some shit

--- web/frontend/src/router/index.js
+++ web/frontend/src/router/index.js
@@ -6,12 +6,18 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta:{
+      title: "Home"
+    }
   },
   {
     path: '/about',
     name: 'about',
-    component: AboutView
+    component: AboutView,
+    meta:{
+      title: "About"
+    }
   }
 ]
 
@@ -19,5 +25,10 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to,from,next)=>{
+  document.title = `${to.meta.title}`;
+  next();
+});
 
 export default router
