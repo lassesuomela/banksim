3d36c1fe8aa84ea58af8d25b300a80bb96530e31
Tommukka <laine.tomi01@gmail.com>
2022-03-23 19:53:23 +0200

    fixes

--- web/frontend/src/router/index.js
+++ web/frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from "../views/Login.vue"
+import Home from '../views/home.vue'
+import Login from "../views/login.vue"
 
 
 
@@ -15,7 +15,7 @@ const routes = [
   },
   {
     path: '/login',
-    name: 'Login',
+    name: 'hogin',
     component: Login,
     meta:{
       title: "Login"
