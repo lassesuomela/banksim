88bea2b4bfe4b3ea48f328e9576defc9f89277c1
Tommukka <laine.tomi01@gmail.com>
2022-03-24 18:25:40 +0200

    connect-history-api-fallback for vue

--- web/backend/app.js
+++ web/backend/app.js
@@ -3,7 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var history = require('connect-history-api-fallback');
+const mysql = require("mysql");
+require("dotenv").config();
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DB
+})
 
+const vue = express.static("../frontend/dist");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,7 +28,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(vue);
+app.use(history({
+  index: "index.html"
+}));
 
+app.use(vue);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
