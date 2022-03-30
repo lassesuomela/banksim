968b15ceaf9940a7c9a8df0dd7bd0f4eb32ce11d
Lasse Suomela <suomelasse@gmail.com>
2022-03-30 14:17:26 +0300

    added check if user has card that they are trying to modify, create card func now takes account id and user id from db, added jwt middleware, changed endpoint names

--- web/backend/models/cardModel.js
+++ web/backend/models/cardModel.js
@@ -9,13 +9,16 @@ const card = {
     getByNumber: function(card_number, callback){
         return db.query("SELECT * FROM card WHERE card_number = ?", [card_number], callback);
     },
+    getByUserID: function(user_ID, callback){
+        return db.query("SELECT * FROM card WHERE user_ID = ?", [user_ID], callback);
+    },
     updateActiveStatus: function(active, card_number, callback){
         return db.query("UPDATE card SET active = ? WHERE card_number = ?", [active, card_number], callback);
     },
-    add: function(req, callback){
+    add: function(req, accountId, callback){
         bcrypt.hash(req.body.pin, saltRounds, function(err, hash){
             return db.query("INSERT INTO card (pin, card_number, account_ID, user_ID, card_type) VALUES(?,?,?,?,?)",
-            [hash, req.body.card_number, req.body.account_ID, req.body.user_ID, req.body.card_type], callback);
+            [hash, req.body.card_number, accountId, req.userId, req.body.card_type], callback);
         });
     },
     delete: function(req, callback){
