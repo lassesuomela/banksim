e7b3b5c84c0aea0c7b8d38a669342084cefeffb6
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-04-05 11:47:15 +0300

    account page update2

--- web/frontend/src/views/accounts.vue
+++ web/frontend/src/views/accounts.vue
@@ -10,21 +10,7 @@
 </template>
 
 <script>
-    export default {
-        name: "Dashboard",
-        data() {
-            return {
-                fname: "",
-                activityLog: [
-                    {title: 'acc 1', text: 'montako', amount: 5000, icon: 'mdi-account', color: 'cyan lighten-3'},
-                    {title: 'acc 2', text: 'id', amount: 3433, icon: 'mdi-account-group-outline', color: 'green darken-2'},
-                    {title: 'acc 3', text: 'joku', amount: 3433, icon: 'mdi-account-group-outline', color: 'blue-grey darken-1'},
 
-                ],
-            }
-        },       
-    }
-    
 </script>
 
 <style scoped>
