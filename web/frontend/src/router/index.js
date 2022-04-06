e46678bbff5b598c0bc572d9286b36d1be342ac9
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-04-06 12:01:03 +0300

    account add popup1

--- web/frontend/src/router/index.js
+++ web/frontend/src/router/index.js
@@ -5,7 +5,6 @@ import HomeComponent from "../views/home.vue"
 import RegisterComponent from "../views/register.vue"
 import LogoutComponent from "../views/logout.vue"
 import AccountsComponent from "../views/accounts.vue"
-import TestComponent from "../views/test.vue"
 import CardsComponent from "../views/cards.vue"
 import axios from "../axios"
 
@@ -22,12 +21,6 @@ const routes = [
         component: LoginComponent
         
     },
-    {
-      path: "/test",
-      name: "test",
-      component: TestComponent
-      
-  },
     {
         path: "/home",
         name: "Home",
