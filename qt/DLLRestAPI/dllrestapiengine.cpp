89181204259b4191d1ed183fc2db83d74d0b0254
Lasse Suomela <suomelasse@gmail.com>
2022-04-13 12:44:25 +0300

    more changes to remove duplicate get tries

--- qt/DLLRestAPI/dllrestapiengine.cpp
+++ qt/DLLRestAPI/dllrestapiengine.cpp
@@ -31,8 +31,6 @@ void DLLRestAPIEngine::Login(QString card, QString pin)
 
 void DLLRestAPIEngine::loginSlot(QNetworkReply *reply)
 {
-
-
     QByteArray response_data=reply->readAll();
     QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
     QJsonObject json_obj = json_doc.object();
@@ -54,12 +52,14 @@ void DLLRestAPIEngine::loginSlot(QNetworkReply *reply)
         manager->deleteLater();
     }else if(status == "error"){
         qDebug() << this->resMessage;
+        tries--;
+        emit SendTriesSignal(tries);
         //this->GetTries(card_number);
         //reply->deleteLater();
         //manager->deleteLater();
     }else{
         qDebug()<<"Wrong pin code"<<json_obj;
-        this->GetTries(card_number);
+        //this->GetTries(card_number);
         reply->deleteLater();
         manager->deleteLater();
     }
